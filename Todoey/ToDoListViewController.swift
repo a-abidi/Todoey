ab2228c17f6199ebf3f9a0d1c092d41dd5f88773
a-abidi <aliabidi@icloud.com>
2020-09-11 12:06:10 +0100

    Add checked and unchecked todo item functionality.

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -17,6 +17,7 @@ class ToDoListViewController: UITableViewController {
     
     let itemArray = ["do thing 1", "do thing 2", "do thing 3"]
 
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         itemArray.count
     }
@@ -27,6 +28,23 @@ class ToDoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+                
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark) {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK:- Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
     }
 
 }
